d34b03dcfe8ed211d23346ff60800dadae351880
Alvaro Arribas <alvaro.arribas@gmail.com>
2023-09-30 22:22:57 +0200

    acabada sección con todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunk',
+    power: 10
+  }]
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string):void{
+    if (!id) {return;}
+    this.onDelete.emit(id);
+
+  }
+  // onDeleteCharacter(index:number):void{
+  //   this.onDelete.emit(index);
+  // }
+
+}
+
+
+function uuid(): string | undefined {
+  throw new Error('Function not implemented.');
+}
+
